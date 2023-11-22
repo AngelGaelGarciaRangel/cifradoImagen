3f9ba3824b13c4555f425f3c10c8a5ec21d8d9d1
gaelgarcia003 <88355164+gaelgarcia003@users.noreply.github.com>
2023-11-21 20:29:15 -0600

    agregué algoritmo de desencriptacion

--- cifradoImagen.py
+++ cifradoImagen.py
@@ -1,15 +1,32 @@
-def encriptar(url, llave, archivo):
+def encriptar(url, llave):
+    print("El url original es", url)
+    print("La llave de encriptación es", llave)
+    archivoLeer = open(url, 'rb')
+    imagen = archivoLeer.read()
+    archivoLeer.close()
+    imagen = bytearray(imagen)
+    for indice, valor in enumerate(imagen):
+        imagen[indice] = valor ^ llave
+    archivoLeer = open(url, 'wb')
+    archivoLeer.write(imagen)
+    archivoLeer.close()
+    print('Encryption Done...')
+
+def desencriptar(url, llave):
   print("El url original es", url)
-  print("La llave de encriptación es", llave)
+  print("La llave de desencriptación es", llave)
   archivoLeer = open(url, 'rb')
   imagen = archivoLeer.read()
   archivoLeer.close()
   imagen = bytearray(imagen)
   for indice, valor in enumerate(imagen):
-	  imagen[indice] = valor ^ llave
+        imagen[indice] = valor ^ llave
   archivoLeer = open(url, 'wb')
-  fin.write(image)
-	fin.close()
-	print('Encryption Done...')
- 
+  archivoLeer.write(imagen)
+  archivoLeer.close()
+  print('Decryption Done...')
+url = input("Ingresa el url de la imagen: ")
+llave = int(input("Ingresa la llave:"))
+desencriptar(url, llave)
+
 #Referencias: Patil, R. (2022). Encrypt and Decrypt Image using Python. Geeksforgeeks. Recuperado de: https://www.geeksforgeeks.org/encrypt-and-decrypt-image-using-python/ 
